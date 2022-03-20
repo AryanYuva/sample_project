165152a6b77e784bb04ed10f7a3508cb4b628129
Aryan <araj38845@gmail.com>
2022-03-20 10:54:58 +0530

    commit

---
+++ exercise_05_tuples.py
@@ -0,0 +1,8 @@
+print("exercise 05 on tuples for loop, book python crash course")
+restrafood = ('aalo dum','panner butter masala','pulao','dal makhni','chapatti')
+for item in restrafood:
+    print(item)
+    
+restrafood = ('panner chilli','panner butter masala','manturian','friedrice','roti')
+for item in restrafood:
+    print(item)
