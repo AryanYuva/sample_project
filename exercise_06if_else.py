86c5a1ca2d04be734ea38c14696314728a3a01ed
Aryan <araj38845@gmail.com>
2022-03-20 16:01:00 +0530

    commit

---
+++ exercise_06if_else.py
@@ -0,0 +1,30 @@
+print("exercise 06 if else conditional statement'book python crash course")
+car = 'sabaru'
+if car=='sabaru':
+    print("True")
+else:
+    print("False")
+age_01 = 18
+age_02 = 21
+if age_01 == 18:
+    print(True)
+else:
+    print(False)
+if age_02 <= age_01:
+    print(True)
+else:
+    print(False)
+if age_01 >= 15 or age_02 >= 15:
+    print(True)
+else:
+    print(False)
+if age_01 >= 15 and age_02 <= 15:
+    print(True)
+else:
+    print(False)
+cars = ['audi','bmw','mahindra','maruti suzuki''hyundai']
+for car in cars:
+    if car == 'audi':
+        print(car.upper())
+    else:
+        print(car.title())
