eeb45ee27fc3fe067b7692a7127f7bd05014dad4
Aryan <araj38845@gmail.com>
2022-03-20 17:50:51 +0530

    commit

---
+++ exercise_5_7.py
@@ -0,0 +1,11 @@
+print("exercise 5.7 if else statement in list,book python crash course")
+fav_fruits = ['mango','apple','grapes']
+for frut in fav_fruits:
+    if frut == 'mango':
+        print('you really like mangoes')
+    if frut == 'lichi':
+        print('not present in list')
+    if frut == 'apple':
+        print("you really like aaple")
+    if frut == 'guava':
+        print("not present in list")
