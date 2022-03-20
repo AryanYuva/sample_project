1ef2514b6d57d8802208f4ff3b84d5be273cfa3b
Aryan <araj38845@gmail.com>
2022-03-20 09:53:21 +0530

    commit

--- exercise_02for_loop_in_list.py
+++ exercise_02for_loop_in_list.py
@@ -1,9 +1,15 @@
 print("exercise 02 python crash course book,for loop in list ")
-pizzas = ['chesse pizza','chicken pizza','panner pizza']
+mypizzas = ['chesse pizza','chicken pizza','panner pizza']
 animals = ['dog','cat','cow']
-for pizzaname in pizzas:
+for pizzaname in mypizzas:
     print("i like" + " " + pizzaname)
 for animal in animals:
     print("i would like to pet a" + " " + animal)
 print("all off these are domectic animal")
-
+friendpizzas = mypizzas[ : ]
+print(mypizzas)
+print(friendpizzas)
+mypizzas.append('tigga pizza')
+friendpizzas.append('chicken chesse pizza')
+print(mypizzas)
+print(friendpizzas)
