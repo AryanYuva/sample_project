0227b8091852c167446b79f1e865f266f4c3060b
Aryan <araj38845@gmail.com>
2022-03-20 09:36:04 +0530

    initial commit

---
+++ exercise_04_list_slices.py
@@ -0,0 +1,8 @@
+print("exercise 04 list slice,book python crash course")
+numlist = []
+for i in range(1,16):
+    numlist.append(i)
+print(numlist)
+print("the first five items in the list are")
+for i in numlist[0:5]:
+    print(i)
