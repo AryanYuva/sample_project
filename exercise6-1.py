b9d34606e96032136eb18f831f4ce72bab3e22ff
Aryan <araj38845@gmail.com>
2022-03-22 10:38:13 +0530

    commit

---
+++ exercise6-1.py
@@ -0,0 +1,12 @@
+print("exercise 6-1 dictionary, book python crash course")
+infodict = {'first_name': 'mahesh' ,'last_name': 'babu','age': '18','city':'ranchi'}
+print(infodict['first_name'])
+print(infodict['last_name'])
+print(infodict['age'])
+print(infodict['city'])
+favno = {'mahesh':'23','surya':'3','varun':'5','prabhas':'13','ranbeer':'21'}
+print("favorite no of mahesh is"+" "+favno['mahesh'])
+print("favorite no of surya is"+" "+favno['surya'])
+print("favorite no of varun is"+" "+favno['varun'])
+print("favorite no of prabhas is"+" "+favno['prabhas'])
+print("favorite no of ranbeer is"+" "+favno['ranbeer'])
