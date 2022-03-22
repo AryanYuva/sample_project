67b3c23c1ade1231216989ece22d67b14b96751e
Aryan <araj38845@gmail.com>
2022-03-22 20:02:41 +0530

    commit

--- exercise 7-4.py
+++ exercise 7-4.py
@@ -1,7 +1,9 @@
 print("Enter the 7-4 while loop pizza toppings,book python crash course")
 message = " "
-while message != 'quit':
+active = True
+while active:
     message = input("Enter the toppings youn want in your pizza\n")
     if message != 'quit':
         print("We are adding" + " " + message + " " + "toppings on your pizza")
-
+    elif message == 'quit':
+        active = False
