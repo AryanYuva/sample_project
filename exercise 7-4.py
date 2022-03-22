37e21a58e53a499bd9afb1b686bc3269589741b0
Aryan <araj38845@gmail.com>
2022-03-22 18:25:58 +0530

    commit

---
+++ exercise 7-4.py
@@ -0,0 +1,7 @@
+print("Enter the 7-4 while loop pizza toppings,book python crash course")
+message = " "
+while message != 'quit':
+    message = input("Enter the toppings youn want in your pizza\n")
+    if message != 'quit':
+        print("We are adding" + " " + message + " " + "toppings on your pizza")
+
