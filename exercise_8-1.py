f22a61b14564dbcb91f05618d013fff306a4bb02
Aryan <araj38845@gmail.com>
2022-03-23 12:22:55 +0530

    commit

---
+++ exercise_8-1.py
@@ -0,0 +1,8 @@
+print("Exercise question 8-1 functions,book python crash course")
+def display_message():
+    print("Hello everyone, we are going to learn about functions in this chapter")
+display_message()
+def favourate_book(title):
+    print("One of my favorate book is" + " " + title + " " + "in wonderland")
+favourate_book('lucent')
+
