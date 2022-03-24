d8aa480d10ffb58060d41bdd5a6d96d5063639bc
Aryan <araj38845@gmail.com>
2022-03-24 13:13:46 +0530

    commit

--- exercise_8-15_part_2.py
+++ exercise_8-15_part_2.py
@@ -1,3 +1,20 @@
-import exercise8_15
+import exercise8_15 
 exercise8_15.print_models('charu','nihu','natasha')
+#exercise 8-16
+#second way of importing
+from exercise8_15 import print_models
+print_models('shumbham','surya','ram charan')
+
+#another way of importing
+
+from exercise8_15 import print_models as pm
+pm('mahesh','allu','shruti')
+
+#another way of importing 
+
+import exercise8_15 as E
+E.print_models('aaliya','john','akshay')
+
+from exercise8_15 import *
+print_models('rajnikant','satrudhan','vivek obroy')
 
